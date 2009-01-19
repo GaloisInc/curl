7edee7c9da637ad42986d29c508676711814b27a
Sigbjorn Finne <sof@galois.com>
2009-01-18 19:24:58 -0800

    documented + added version number wrappers

--- curlc.c
+++ curlc.c
@@ -1,3 +1,12 @@
+/*
+ * Haskell FFI friendly wrappers to curl_easy_* functions
+ * for setting/getting option values. Could import these into
+ * .hs without too much trouble, but calling out to 'typed'
+ * versions saves the C compiler from issuing warnings.
+ *
+ * (c) 2007-2009, Galois, Inc.
+ *
+ */
 #include <curl/curl.h>
 
 int curl_easy_getinfo_long(void *curl, long tg, long *pl)
@@ -32,3 +41,29 @@ int curl_easy_setopt_string(void *curl, int i, char *x)
 
 int curl_easy_setopt_ptr(void *curl, int i, void *x)
 { return curl_easy_setopt(curl,i,x); }
+
+/*
+ * Function curl_version_str()
+ *
+ * Returns the libcurl version number as a "MAJOR.MINOR.PATCH" string.
+ *
+ * Note: a static string, so no free()ing required (or asked for! :-)
+ */
+char*
+curl_version_str() {
+  return LIBCURL_VERSION;
+}
+
+/*
+ * Function curl_version_num()
+ *
+ * Returns the libcurl version number in 3-byte format 0xXXYYZZ,
+ * representing major,minor and patch levels. Encoded in a (host)
+ * 'int' value, making for easy comparisons.
+ *
+ * See curlver.h for complete story.
+ */
+int
+curl_version_num() {
+  return LIBCURL_VERSION_NUM;
+}
